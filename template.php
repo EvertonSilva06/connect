ff391d561c906033a87f94590b8d976ae4d2aafd
EvertonSilva06 <evertondasilva670@gmail.com>
2024-10-08 21:29:18 -0300

    so o cabeçalho zuado mas ta ai

--- template.php
+++ template.php
@@ -4,10 +4,11 @@
         <h2>Nome:<?= $funcionarios['nome']?></h2>
         <p>Cargo:<?= $funcionarios['cargo']?> </p>
         <p>Idade:<?= $funcionarios['idade']?> </p>
-       
+        <p>Salario: R$<?= $funcionarios['salario']?></p>
+        <P>Telefone: <?= $funcionarios['telefone']?></P>
     </header>
     <footer>
-        <a href="tel">Telefone: <?= $funcionarios['telefone']?></a>
-        <span>ID: <?=$funcionarios['id']?></span>
+        
+        <a href="#"><span>ID: <?=$funcionarios['id']?></span></a>
     </footer>
 </div>
